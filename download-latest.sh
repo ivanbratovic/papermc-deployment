f2b74ce8454e9efe5ddf2f778c67ed521896e11b
Ivan Bratović <ivanbratovic4@gmail.com>
2025-08-09 00:21:51 +0200

    Fix download-latest.sh to stop correctly

--- download-latest.sh
+++ download-latest.sh
@@ -4,7 +4,7 @@ PROJECT="$1"
 MINECRAFT_VERSION="$2"
 
 LATEST_BUILD=$(curl -s https://api.papermc.io/v2/projects/${PROJECT}/versions/${MINECRAFT_VERSION}/builds | \
-    jq -r '.builds | map(select(.channel == "default") | .build) | .[-1]')
+    jq -r '.builds | map(select(.channel == "default") | .build) | .[-1]' 2>/dev/null || echo "null")
 
 if [ "$LATEST_BUILD" != "null" ]; then
     JAR_NAME=${PROJECT}-${MINECRAFT_VERSION}-${LATEST_BUILD}.jar
@@ -19,5 +19,4 @@ else
     echo "No stable build for version $MINECRAFT_VERSION found :("
     exit 1
 fi
-
 
